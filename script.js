37842bb5838894ce20c609da2fbc9fe405a8778c
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-03-28 13:01:19 -0300

    commit exercise

---
+++ script.js
@@ -0,0 +1,25 @@
+//alert('eitaaaa')
+
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+// PARA TESTE var hora = 17
+msg.innerHTML = 'Agora são ' + hora + ' horas'
+if(hora >= 0 && hora < 12){
+    //BOM DIA
+    img.src = 'manha1.png'
+    document.body.style.background = '#ffd876'
+}else if(hora >=12 && hora<18){
+   //BOA TARDE 
+   img.src = 'tarde1.png'
+   document.body.style.background = '#825057'
+}else{
+    //BOA NOITE
+    img.src = 'noite1.png'
+    document.body.style.background = '#131720'
+}
+
+}
+
